1ae83473eb96cdcce309e81eb3c3b87e7e0c9215
gpganeshpatekar <gpganeshpatekar@gmail.com>
2022-08-13 00:15:59 +0530

    SBCWR-3 : Create Entity Class

--- src/main/java/com/demo/springbootcrudwithrestful/model/Employee.java
+++ src/main/java/com/demo/springbootcrudwithrestful/model/Employee.java
@@ -1,5 +1,85 @@
 package com.demo.springbootcrudwithrestful.model;
 
-public class Employee {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Employee {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "EMP_ID")
+	private Integer empId;
+	@Column(name = "EMP_NAME")
+	private String empName;
+	@Column(name = "EMP_AGE")
+	private Integer empAge;
+	@Column(name = "EMP_SALARY")
+	private double empSalary;
+	@Column(name = "EMP_ADDRESS")
+	private String empAddress;
+	@Column(name = "EMP_EMAIL")
+	private String empEmail;
+	@Column(name = "EMP_PASSWORD")
+	private String empPassword;
+	
+	public Integer getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public Integer getEmpAge() {
+		return empAge;
+	}
+	public void setEmpAge(Integer empAge) {
+		this.empAge = empAge;
+	}
+	public double getEmpSalary() {
+		return empSalary;
+	}
+	public void setEmpSalary(double empSalary) {
+		this.empSalary = empSalary;
+	}
+	public String getEmpAddress() {
+		return empAddress;
+	}
+	public void setEmpAddress(String empAddress) {
+		this.empAddress = empAddress;
+	}
+	public String getEmpEmail() {
+		return empEmail;
+	}
+	public void setEmpEmail(String empEmail) {
+		this.empEmail = empEmail;
+	}
+	public String getEmpPassword() {
+		return empPassword;
+	}
+	public void setEmpPassword(String empPassword) {
+		this.empPassword = empPassword;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empAge=" + empAge + ", empSalary=" + empSalary
+				+ ", empAddress=" + empAddress + ", empEmail=" + empEmail + ", empPassword=" + empPassword + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
 }
