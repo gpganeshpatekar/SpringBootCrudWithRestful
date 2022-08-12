baecf2fc58b1798ba8c19cd64be5fb3172d41afb
gpganeshpatekar <gpganeshpatekar@gmail.com>
2022-08-12 16:11:30 +0530

    Project added to central repository

---
+++ src/test/java/com/demo/springbootcrudwithrestful/SpringBootCrudWithRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootcrudwithrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudWithRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
