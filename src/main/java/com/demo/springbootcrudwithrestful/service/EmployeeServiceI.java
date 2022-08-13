6d94f8011a7dbb1fe26063e37cedcdc76bd401be
gpganeshpatekar <gpganeshpatekar@gmail.com>
2022-08-13 12:54:05 +0530

    SBCWR-8 : Delcare Method In ServiceI

--- src/main/java/com/demo/springbootcrudwithrestful/service/EmployeeServiceI.java
+++ src/main/java/com/demo/springbootcrudwithrestful/service/EmployeeServiceI.java
@@ -1,5 +1,20 @@
 package com.demo.springbootcrudwithrestful.service;
 
+import java.util.List;
+
+import com.demo.springbootcrudwithrestful.model.Employee;
+
 public interface EmployeeServiceI {
+	
+	public Employee employeeLogin(Employee employee);
+	public Employee addEmployee(Employee employee);
+	public List<Employee> addMultipleEmployee(List<Employee> employee);
+	public List<Employee> getAllEmployee();
+	public Employee getEmployeeById(Integer empId);
+	public List<Employee> getEmployeeByAge(Integer empAge);
+	public Employee updateEmployee(Employee employee);
+	public Employee updateAllEmployee(Employee employee);
+	public Employee deleteEmployee(Integer empId);
+	public Employee deleteAllEmployee(Employee employee);
 
 }
